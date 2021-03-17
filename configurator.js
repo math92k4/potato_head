3fd0bbcc1906b6673164e4ea065f5955e9d7cdbd
Mathias <math92k4@stud.kea.dk>
2021-03-17 15:08:29 +0100

    inital setup for catergory click. needs its own js file

--- configurator.js
+++ configurator.js
@@ -6,7 +6,35 @@ import { loadSvg } from "./fetch_svg.js";
 window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
+  //load and display main-svg
   const url = "imgs/potatohead_svg/potato_head_cropped-01.svg";
   const object = document.querySelector("#character");
   loadSvg(url, object);
+
+  setUpCategoryLister();
+}
+
+function setUpCategoryLister() {
+  const categories = document.querySelectorAll(".category_btn");
+
+  categories.forEach((category) => {
+    category.addEventListener("click", catergoryClicked);
+  });
+}
+
+function catergoryClicked() {
+  hideAllCategories();
+
+  //adds class "show" to the clicked category dropdown
+  const categoryDropdown = this.nextElementSibling;
+  categoryDropdown.classList.add("show");
+  console.log(categoryDropdown);
+}
+
+function hideAllCategories() {
+  const allDropdowns = document.querySelectorAll(".dropdown");
+  allDropdowns.forEach((dropdown) => {
+    dropdown.classList = "dropdown";
+    console.log(dropdown);
+  });
 }
