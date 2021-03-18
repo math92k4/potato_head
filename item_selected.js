e8b07333d17bd5752e6a093491ef41d398d1677c
Morten Gross <mg@vuffeli.dk>
2021-03-18 13:58:26 +0100

    this might work...

--- item_selected.js
+++ item_selected.js
@@ -1,41 +1,40 @@
 "use strict";
 
-const nose = {
+const features = {
   red_nose: false,
   dark_nose: false,
   pale_nose: false,
-};
-
-const hat = {
   black_hat: false,
   white_hat: false,
-};
-
-const arms = {
   arms: false,
-};
-
-const mouth = {
   lips: false,
   mustache_mouth: false,
   teeth: false,
-};
-
-const eyes = {
   os_eyes: false,
   ms_eyes: false,
   lazy_eyes: false,
-};
-
-const shoes = {
   mr_shoes: false,
-  ms_shoes: false,
-};
+  ms_shoes: false
+}
+
 
 export function toggleOption(event) {
   const target = event.currentTarget;
   const option = target.dataset.name;
   const optionCategory = target.dataset.type;
 
-  console.log(option + "   " + optionCategory);
+  console.log(optionCategory);
+
+  if (features[option]) {
+    features[option] = false;
+  } else {
+    features[option] = true;
+    document.querySelectorAll(`[data-type=${optionCategory}`).forEach(type => {
+      type.dataset.name = false;
+    })
+    
+    features[optionCategory]
+  }
+  console.log(features[option]);
+
 }
