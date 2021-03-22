667cafee32ad02417aa88ff8f4f3777adf57237c
Mathias <math92k4@stud.kea.dk>
2021-03-22 18:50:42 +0100

    scroll.html now loads the svg colors

--- scroll.js
+++ scroll.js
@@ -16,9 +16,6 @@ async function init() {
   //splits string to array by ","
   const arrayOfIds = stringToArray(stringOfIds, ",");
 
-  //get color values - if any
-  const svgColors = arrayOfIds.filter(filterColors);
-
   //for each svgPart, detemerine to show or not
   const allOptionParts = document.querySelectorAll(".hide");
   allOptionParts.forEach((part) => {
@@ -46,14 +43,43 @@ function determineShow(part, arrayOfIds) {
   arrayOfIds.forEach((paramsId) => {
     let idForMatch = paramsId;
 
-    if (paramsId.includes("#")) {
-      const onlyColor = getSvgColor(paramsId);
-      idForMatch = getId();
+    if (idForMatch.includes("-")) {
+      const svgColor = getSvgColor(paramsId);
+      setColor(part, svgColor);
+      idForMatch = getOnlyId(paramsId);
     }
+    console.log(idForMatch);
+
     if (idForMatch === partId) {
       part.classList = "show";
     }
   });
 
-  console.log(part);
+  /*
+   *
+   *
+   * closure
+   */
+  function getSvgColor(paramsId) {
+    const colorStart = paramsId.indexOf("-");
+    const colorString = paramsId.substring(colorStart + 1);
+    const result = "#" + colorString;
+
+    return result;
+  }
+
+  function getOnlyId(paramsId) {
+    const idEnd = paramsId.indexOf("-");
+    const idString = paramsId.substring(0, idEnd);
+
+    return idString;
+  }
+
+  function setColor(part, svgColor) {
+    const paths = part.querySelectorAll("polygon");
+
+    paths.forEach((path) => {
+      path.style.setProperty("--svg_color", svgColor);
+    });
+  }
 }
