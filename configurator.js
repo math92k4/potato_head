add9593c167c4bebb17fc24ab1f15e357a3538d8
Mathias <math92k4@stud.kea.dk>
2021-03-22 09:31:12 +0100

    config js & html updated

--- configurator.js
+++ configurator.js
@@ -4,6 +4,7 @@
 import { loadSvg } from "./fetch_svg.js";
 import { optionsMenuSetUp } from "./options_menu.js";
 import { toggleOptions } from "./item_selected.js";
+import { scrollPageBtnClicked } from "./click_to_scroll.js";
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -20,4 +21,6 @@ async function init() {
   options.forEach((option) => {
     option.addEventListener("click", toggleOptions);
   });
+
+  document.querySelector("#scroll_p_btn").addEventListener("click", scrollPageBtnClicked);
 }
